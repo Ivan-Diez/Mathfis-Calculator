babc0b688f91d4a051fcc8722e9c898852e91cbb
Ivan Diez <ivangdiez.arg@gmail.com>
2022-09-05 20:44:31 -0300

    Agregué array y métodos

--- app.js
+++ app.js
@@ -3,121 +3,201 @@ function conversor(numeroCentimetros) {
   return numeroCentimetros / 100;
 }
 
+//DECLARACIÓN DE FUNCIONES
+const calcularAreaTriangulo = (base, altura) => {
+  return (area = (base * altura) / 2);
+};
+const calcularPerimetroTriangulo = (base, altura, diagonal) => {
+  return (perimetro = base + altura + diagonal);
+};
+
+//DECLARACIÓN DE OBJETOS
+
+const misFiguras = [];
+
+const triangulo = {
+  base: "",
+  altura: "",
+  diagonal: "",
+  calcularArea: function () {
+    return (this.base * this.altura) / 1;
+  },
+  calcularPerimetro: function () {
+    return this.base + this.altura + this.diagonal;
+  },
+};
+
+const paralelogramos = {
+  base: "",
+  altura: "",
+  calcularArea: function () {
+    return this.base * this.altura;
+  },
+  calcularPerimetro: function () {
+    return (this.base + this.altura) * 2;
+  },
+};
+
+const circulo = {
+  radio: "",
+  calcularArea: function () {
+    return this.radio * this.radio * Math.PI;
+  },
+  calcularPerimetro: function () {
+    return 2 * this.radio * Math.PI;
+  },
+};
+
+// COMIENZO DE LA APLICACIÓN
 alert(
   "Bienvenidos a Mathfis Calculator, la aplicación web que te permitirá resolver tus problemas físicos y matemáticos brindandote las herramientas que automatizarán el cálculo de tus incógnitas !"
 );
 
 alert(
-  "Elegí que figura queres analizar, en caso de no querer continuar escribí FINALIZAR"
+  "Elegí que figura queres analizar, en caso de no querer continuar escribí finalizar"
 );
 
-alert("TRIANGULO, CUADRADO, RECTANGULO, CIRCULO o FINALIZAR");
-let figura = prompt("Ingresá el nombre de la figura:");
+alert(
+  "Por favor, respete las mayúsculas: triangulo, paralelogramos, circulo o finalizar"
+);
 
-// ALGORITMO CON CICLO
+let figura = prompt(
+  "Ingrese el nombre de la figura que desea analizar en la calculadora de Mathfis. En caso de no querer realizar una consulta ingrese la palabra finalizar"
+);
 
-while (figura != "FINALIZAR") {
-  let triangulo = "TRIANGULO";
-  let cuadrado = "CUADRADO";
-  let rectangulo = "RECTANGULO";
-  let circulo = "CIRCULO";
+const FIGURAS = {
+  triangulo: "triangulo",
+  paralelogramos: "paralelogramos",
+  circulo: "circulo",
+};
 
-  if (figura === triangulo) {
-    let base = Number(prompt("Ingresá la longitud de la base del triángulo"));
-    let altura = Number(
+const seleccion = FIGURAS[figura];
+
+while (figura != "finalizar") {
+  // FIGURA: TRIANGULO
+  if (seleccion === "triangulo") {
+    triangulo.base = Number(
+      prompt("Ingresá la longitud de la base del triangulo")
+    );
+    triangulo.altura = Number(
       prompt("Ingresá la longitud de la altura del triangulo")
     );
-    let diagonal = Number(
-      prompt("Ingresá la longitud de la diagonal del triangulo")
-    );
-    if (base != 0 && altura != 0 && diagonal != 0) {
-      let area = (base * altura) / 2;
-      let perimetro = base + altura + diagonal;
+    triangulo.diagonal = Number(prompt("Ingresá la longitud de la diagonal"));
+    if (triangulo.base > 0 && triangulo.altura > 0 && triangulo.diagonal > 0) {
+      triangulo.calcularArea();
+      triangulo.calcularPerimetro();
       alert(
-        `El area del triangulo está compuesta de ${area} centímetros cuadrados`
-      );
-      alert(`El perimetro del triangulo es de ${perimetro} centímetros`);
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud."
-      );
-    }
-  } else if (figura === cuadrado) {
-    let lado = Number(
-      prompt("Ingresá la longitud de uno de los lados del cuadrado:")
-    );
-    if (lado != 0) {
-      let area = lado * lado;
-      let perimetro = lado * 4;
-      alert(
-        `El area del cuadrado está compuesta de ${area} centímetros cuadrados`
-      );
-      alert(`El perimetro del cuadrado es de ${perimetro} centímetros`);
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud."
-      );
-    }
-  } else if (figura === rectangulo) {
-    let base = Number(prompt("Ingresá la longitud de la base del rectángulo"));
-    let altura = Number(
-      prompt("Ingresá la longitud de la altura del rectángulo")
-    );
-    if (base != 0 && altura != 0) {
-      let area = base * altura;
-      let perimetro = (base + altura) * 2;
-      alert(
-        `El area del rectángulo está compuesta de ${area} centímetros cuadrados`
-      );
-      alert(`El perimetro del rectángulo es de ${perimetro} centímetros`);
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud."
-      );
-    }
-  } else if (figura === circulo) {
-    let radio = prompt("Ingresá el radio del circulo:");
-    if (radio != 0) {
-      const pi = Math.PI;
-      let area = pi * radio * radio;
-      let perimetro = 2 * pi * radio;
-      let areaDosDecimales = area.toFixed(2);
-      let perimetroDosDecimales = perimetro.toFixed(2);
-      alert(
-        `El area del círculo está compuesta de ${areaDosDecimales} centímetros cuadrados`
+        `El area del triangulo es de ` +
+          triangulo.calcularArea() +
+          ` centímetros cuadrados`
       );
       alert(
-        `El perimetro del círculo es de ${perimetroDosDecimales} centímetros`
+        `El perimetro del triangulo es de ` +
+          triangulo.calcularPerimetro() +
+          ` centímetros`
+      );
+      // USO DEL METODO PUSH - ARRAYS
+      misFiguras.push(
+        `La figura analizada fue un triangulo`,
+        "\nEl area es de: " +
+          Math.round(triangulo.calcularArea()) +
+          " centimetros cuadrados",
+        "\nEl períemtro es de: " +
+          Math.round(triangulo.calcularPerimetro()) +
+          " centimetros"
       );
       break;
     } else {
       alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud."
+        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
       );
     }
-  } else {
-    alert(
-      "No ingresaste una figura correcta, por favor reiniciá la aplicación y revisa nuevamente las figuras disponibles para su análisis."
+  }
+  // FIGURA: PARALELOGRAMOS
+  else if (seleccion === "paralelogramos") {
+    let figura = prompt(
+      "Ingrese el tipo de paralelogramo: cuadrado, rectangulo"
     );
-    break;
+    if (figura === "cuadrado") {
+      paralelogramos.base = Number(
+        prompt(`Ingresá la longitud de un lado del ${figura}`)
+      );
+      paralelogramos.altura = paralelogramos.base;
+    } else if (figura === "rectangulo") {
+      paralelogramos.base = Number(
+        prompt(`Ingresá la longitud de la base del ${figura}`)
+      );
+      paralelogramos.altura = Number(
+        prompt(`Ingresá la longitud de la altura del ${figura}`)
+      );
+    }
+    if (paralelogramos.base > 0 && paralelogramos.altura > 0) {
+      paralelogramos.calcularArea();
+      paralelogramos.calcularPerimetro();
+      alert(
+        `El area del ${figura} es de ` +
+          paralelogramos.calcularArea() +
+          ` centímetros cuadrados`
+      );
+      alert(
+        `El perimetro del ${figura} es de ` +
+          paralelogramos.calcularPerimetro() +
+          ` centímetros`
+      );
+      misFiguras.push(
+        `La figura analizada fue un ${figura}`,
+        "\nEl area es de: " +
+          Math.round(paralelogramos.calcularArea()) +
+          " centimetros cuadrados",
+        "\nEl períemtro es de: " +
+          Math.round(paralelogramos.calcularPerimetro()) +
+          " centimetros"
+      );
+      break;
+    } else {
+      alert(
+        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
+      );
+    }
+  }
+  // FIGURA: CIRCULO
+  else if (seleccion === "circulo") {
+    circulo.radio = Number(prompt("Ingresá el valor de radio del circulo"));
+
+    if (circulo.radio > 0) {
+      circulo.calcularArea();
+      circulo.calcularPerimetro();
+      alert(
+        `El area del circulo es de ` +
+          Math.round(circulo.calcularArea()) +
+          ` centimetros cuadrados aproximadamente`
+      );
+      alert(
+        `El perimetro del circulo es de ` +
+          Math.round(circulo.calcularPerimetro()) +
+          ` centimetros aproximadamente`
+      );
+      misFiguras.push(
+        "La figura analizada fue un Circulo",
+        "\nEl area es de: " +
+          Math.round(circulo.calcularArea()) +
+          " centimetros cuadrados",
+        "\nEl períemtro es de: " +
+          Math.round(circulo.calcularPerimetro()) +
+          " centimetros"
+      );
+      break;
+    } else {
+      alert(
+        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
+      );
+    }
   }
 }
 
-let numero = prompt(
-  "Si te interesa obtener el perímetro calculado en unidad de metros ingresá su valor. En caso de no estar interesado escribí FINALIZAR"
-);
-
-while (numero != "FINALIZAR") {
-  for (let i = 0; i <= 5; i++) {
-    if (numero > 0) {
-      alert("El valor es de" + " " + conversor(numero) + " " + "metros");
-    } else {
-      alert("Ingresaste un valor incorrecto, por favor volvé a intentarlo.");
-    }
-    break;
-  }
-  break;
+if (figura != "finalizar") {
+  alert(misFiguras);
+  alert("Gracias por visitar nuestro sitio web!");
+} else {
+  alert("Gracias por visitar nuestro sitio web!");
 }

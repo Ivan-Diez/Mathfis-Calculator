04cc955254cad5c8c21c9e159fad3ef74eb108ee
Ivan Diez <ivangdiez.arg@gmail.com>
2022-09-14 22:59:26 -0300

    Añado Eventos con input

--- app.js
+++ app.js
@@ -1,233 +1,106 @@
-/*
-// FUNCION
-function conversor(numeroCentimetros) {
-  return numeroCentimetros / 100;
+// Creación de Figuras dinámicas
+const articulo = document.getElementById("figuras");
+const opciones = document.createElement("div");
+opciones.innerHTML = `<button type="button" class="btn btn-secondary" id="triangulo" onClick="datosTriangulo()">Triángulo</button>
+<button type="button" class="btn btn-secondary" id="cuadrado" onClick="datosParalelogramos()">Cuadrado</button>
+<button type="button" class="btn btn-secondary" id="rectangulo" onClick="datosParalelogramos()">Rectángulo</button>
+<button type="button" class="btn btn-secondary" id="circulo" onClick="datosCirculo()">Círculo</button>`;
+articulo.appendChild(opciones);
+
+// Display: Ingresar datos para figura Triangulo
+function datosTriangulo() {
+  opciones.innerHTML = "";
+  let datos = document.createElement("div");
+  datos.innerHTML = `<input class="field" type="text" name="base" id="base" placeholder="Ingrese la base"/>
+   <input class="field" type="text" name="altura" id="altura" placeholder="Ingrese la altura"/>
+   <input class="field" type="text" name="diagonal" id="diagonal" placeholder="Ingrese la diagonal"/>
+   <button type="button" class="btn btn-secondary" onClick="cargarTriangulo()">Calcular</button>`;
+  opciones.appendChild(datos);
 }
 
-//DECLARACIÓN DE OBJETOS
-
-const misFiguras = [];
-
-//
-/
-/
-/
-/
-/
-/
-//
-
-const paralelogramos = {
-  base: "",
-  altura: "",
-  calcularArea: function () {
-    return this.base * this.altura;
-  },
-  calcularPerimetro: function () {
-    return (this.base + this.altura) * 2;
-  },
-};
-
-const circulo = {
-  radio: "",
-  calcularArea: function () {
-    return this.radio * this.radio * Math.PI;
-  },
-  calcularPerimetro: function () {
-    return 2 * this.radio * Math.PI;
-  },
-};
-
-// COMIENZO DE LA APLICACIÓN
-
-const FIGURAS = {
-  triangulo: "triangulo",
-  paralelogramos: "paralelogramos",
-  circulo: "circulo",
-};
-
-const seleccion = FIGURAS[figura];
-
-while (figura != "finalizar") {
-  // FIGURA: TRIANGULO
-  if (seleccion === "triangulo") {
-    triangulo.base = Number(
-      prompt("Ingresá la longitud de la base del triangulo")
-    );
-    triangulo.altura = Number(
-      prompt("Ingresá la longitud de la altura del triangulo")
-    );
-    triangulo.diagonal = Number(prompt("Ingresá la longitud de la diagonal"));
-    if (triangulo.base > 0 && triangulo.altura > 0 && triangulo.diagonal > 0) {
-      triangulo.calcularArea();
-      triangulo.calcularPerimetro();
-      alert(
-        `El area del triangulo es de ` +
-          triangulo.calcularArea() +
-          ` centímetros cuadrados`
-      );
-      alert(
-        `El perimetro del triangulo es de ` +
-          triangulo.calcularPerimetro() +
-          ` centímetros`
-      );
-      // USO DEL METODO PUSH - ARRAYS
-      misFiguras.push(
-        `La figura analizada fue un triangulo`,
-        "\nEl area es de: " +
-          Math.round(triangulo.calcularArea()) +
-          " centimetros cuadrados",
-        "\nEl períemtro es de: " +
-          Math.round(triangulo.calcularPerimetro()) +
-          " centimetros"
-      );
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
-      );
-    }
-  }
-  // FIGURA: PARALELOGRAMOS
-  else if (seleccion === "paralelogramos") {
-    let figura = prompt(
-      "Ingrese el tipo de paralelogramo: cuadrado, rectangulo"
-    );
-    if (figura === "cuadrado") {
-      paralelogramos.base = Number(
-        prompt(`Ingresá la longitud de un lado del ${figura}`)
-      );
-      paralelogramos.altura = paralelogramos.base;
-    } else if (figura === "rectangulo") {
-      paralelogramos.base = Number(
-        prompt(`Ingresá la longitud de la base del ${figura}`)
-      );
-      paralelogramos.altura = Number(
-        prompt(`Ingresá la longitud de la altura del ${figura}`)
-      );
-    }
-    if (paralelogramos.base > 0 && paralelogramos.altura > 0) {
-      paralelogramos.calcularArea();
-      paralelogramos.calcularPerimetro();
-      alert(
-        `El area del ${figura} es de ` +
-          paralelogramos.calcularArea() +
-          ` centímetros cuadrados`
-      );
-      alert(
-        `El perimetro del ${figura} es de ` +
-          paralelogramos.calcularPerimetro() +
-          ` centímetros`
-      );
-      misFiguras.push(
-        `La figura analizada fue un ${figura}`,
-        "\nEl area es de: " +
-          Math.round(paralelogramos.calcularArea()) +
-          " centimetros cuadrados",
-        "\nEl períemtro es de: " +
-          Math.round(paralelogramos.calcularPerimetro()) +
-          " centimetros"
-      );
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
-      );
-    }
-  }
-  // FIGURA: CIRCULO
-  else if (seleccion === "circulo") {
-    circulo.radio = Number(prompt("Ingresá el valor de radio del circulo"));
-
-    if (circulo.radio > 0) {
-      circulo.calcularArea();
-      circulo.calcularPerimetro();
-      alert(
-        `El area del circulo es de ` +
-          Math.round(circulo.calcularArea()) +
-          ` centimetros cuadrados aproximadamente`
-      );
-      alert(
-        `El perimetro del circulo es de ` +
-          Math.round(circulo.calcularPerimetro()) +
-          ` centimetros aproximadamente`
-      );
-      misFiguras.push(
-        "La figura analizada fue un Circulo",
-        "\nEl area es de: " +
-          Math.round(circulo.calcularArea()) +
-          " centimetros cuadrados",
-        "\nEl períemtro es de: " +
-          Math.round(circulo.calcularPerimetro()) +
-          " centimetros"
-      );
-      break;
-    } else {
-      alert(
-        "Ingresaste un valor de 0, es imposible efectuar el cálculo con esa longitud. Por favor, volvé a intentarlo."
-      );
-    }
-  }
+// Display: Ingresar datos para figura Paralelogramos
+function datosParalelogramos() {
+  opciones.innerHTML = "";
+  let datos = document.createElement("div");
+  datos.innerHTML = `<input class="field" type="text" name="base" id="base" placeholder="Ingrese la base"/>
+  <input class="field" type="text" name="altura" id="altura" placeholder="Ingrese la altura"/>
+  <button type="button" class="btn btn-secondary" onClick="cargarParalelogramos()">Calcular</button>`;
+  opciones.appendChild(datos);
 }
 
-if (figura != "finalizar") {
-  alert(misFiguras);
-  alert("Gracias por visitar nuestro sitio web!");
-} else {
-  alert("Gracias por visitar nuestro sitio web!");
+// Display: Ingresar datos para figura Circulo
+function datosCirculo() {
+  opciones.innerHTML = "";
+  let datos = document.createElement("div");
+  datos.innerHTML = `<input class="field" type="text" name="radio" id="radio" placeholder="Ingrese el radio"/>
+  <button type="button" class="btn btn-secondary" onClick="cargarCirculo()">Calcular</button>`;
+  opciones.appendChild(datos);
 }
 
-*/
-
-let article = document.getElementById("figuras");
-let subtitulo = document.getElementById("subtitulo");
-
-let opciones = document.createElement("div");
-opciones.innerHTML = `<button type="button" class="btn btn-secondary" onClick="ingresarDatos()">Triángulo</button>
-<button type="button" class="btn btn-secondary" onClick="mostrarCalculadora()">Cuadrado</button>
-<button type="button" class="btn btn-secondary" onClick="mostrarCalculadora()">Rectángulo</button>
-<button type="button" class="btn btn-secondary" onClick="mostrarCalculadora()">Círculo</button>`;
-article.appendChild(opciones);
-
-//OBJETO TRIANGULO
-
-const triangulo = {
-  base: "",
-  altura: "",
-  diagonal: "",
-  area: "",
-  perimetro: "",
-};
-
-function ingresarDatos() {
-  triangulo.base = prompt("Ingrese la base del triángulo");
-  triangulo.altura = prompt("Ingrese la altura del triángulo");
-  triangulo.diagonal = prompt("Ingrese la diagonal del triángulo");
-  if (triangulo.base > 0 && triangulo.altura > 0 && triangulo.diagonal > 0) {
-    realizarCalculos();
-  } else {
-    mostrarError();
-  }
+// Capturar datos
+// TRIANGULO
+function cargarTriangulo() {
+  let base = Number(document.getElementById("base").value);
+  let altura = Number(document.getElementById("altura").value);
+  let diagonal = Number(document.getElementById("diagonal").value);
+  calcularTriangulo(base, altura, diagonal);
+}
+// PARALELOGRAMOS
+function cargarParalelogramos() {
+  let base = Number(document.getElementById("base").value);
+  let altura = Number(document.getElementById("altura").value);
+  calcularParalelogramos(base, altura);
 }
 
-function realizarCalculos() {
-  triangulo.area = (triangulo.base * triangulo.altura) / 2;
-  triangulo.perimetro = triangulo.base + triangulo.altura + triangulo.diagonal;
-  mostrarResultados();
+// CIRCULO
+function cargarCirculo() {
+  let radio = Number(document.getElementById("radio").value);
+  calcularCirculo(radio);
 }
 
-function mostrarResultados() {
-  article.innerHTML = "";
-  subtitulo.innerHTML = "";
-  let calculador = document.createElement("div");
-  calculador.innerHTML = `<h2>Resultado obtenido:</h2><p class="font-monospace">El area de tu triangulo es de ${triangulo.area} centímetros cuadrados</p><p class="font-monospace">El perímetro de tu triángulo es de ${triangulo.perimetro} centímetros</p>`;
-  article.appendChild(calculador);
+// Calcular area y perímetro
+// TRIANGULO
+function calcularTriangulo(base, altura, diagonal) {
+  let area = (base * altura) / 2;
+  let perimetro = base + altura + diagonal;
+  resultadosTriangulo(area, perimetro);
+}
+// PARALELOGRAMOS
+function calcularParalelogramos(base, altura) {
+  let area = base * altura;
+  let perimetro = (base + altura) * 2;
+  resultadosParalelogramos(area, perimetro);
+}
+// CIRCULO
+function calcularCirculo(radio) {
+  let area = radio * radio * Math.PI;
+  let perimetro = 2 * radio * Math.PI;
+  resultadosCirculo(area, perimetro);
 }
 
-function mostrarError() {
-  article.innerHTML = "";
-  subtitulo.innerHTML = "";
-  let error = document.createElement("div");
-  error.innerHTML = `<h2>Resultado obtenido:</h2><p class="font-monospace">No fue posible efectuar el cálculo dado que uno o más datos de los ingresados tiene un valor de incorrecto, es imposible efectuar el cálculo con ese valor de longitud. Por favor, recargá la página y volvé a intentarlo.</p>`;
-  article.appendChild(error);
+// Mostrar resultados
+// TRIANGULO
+function resultadosTriangulo(area, perimetro) {
+  opciones.innerHTML = "";
+  let resultados = document.createElement("div");
+  resultados.innerHTML = `<p>El area de la figura es de ${area} centímetros cuadrados</p><p>El perimetro de la figura es de ${perimetro} centímetros</p>`;
+  opciones.appendChild(resultados);
+}
+// PARALELOGRAMOS
+function resultadosParalelogramos(area, perimetro) {
+  opciones.innerHTML = "";
+  let resultados = document.createElement("div");
+  resultados.innerHTML = `<p>El area de la figura es de ${area} centímetros cuadrados</p><p>El perimetro de la figura es de ${perimetro} centímetros</p>`;
+  opciones.appendChild(resultados);
+}
+// CIRCULO
+function resultadosCirculo(area, perimetro) {
+  opciones.innerHTML = "";
+  let resultados = document.createElement("div");
+  resultados.innerHTML = `<p>El area de la figura es de ${Math.round(
+    area
+  )} centímetros cuadrados</p><p>El perimetro de la figura es de ${Math.round(
+    perimetro
+  )} centímetros</p>`;
+  opciones.appendChild(resultados);
 }
